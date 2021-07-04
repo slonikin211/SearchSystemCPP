12f66e4b82f4ca6e493e4b9489af58be5c423fe3
slonikin211 <pakgeorge1209@gmail.com>
2021-07-04 14:53:16 +0300

    removed namespace std and 's' after strings

--- src/include/TestSearchServer.hpp
+++ src/include/TestSearchServer.hpp
@@ -1,5 +1,4 @@
-#ifndef TEST_SEARCH_SERVER_HPP
-#define TEST_SEARCH_SERVER_HPP
+#pragma once
 
 #include <algorithm>
 #include <cmath>
@@ -11,60 +10,58 @@
 #include <iostream>
 #include <tuple>
 
-using namespace std;
-
 #include "SearchServer.hpp"     // Класс поисковой системы для тестирования
 
 namespace Test_SearchServer
 {
     // Тест-функция для макроса ASSERT
     template <typename T>
-    void AssertImpl(const T& t, const string& t_str, const string& file, const string& func,
-        unsigned line, const string& hint) 
+    void AssertImpl(const T& t, const std::string& t_str, const std::string& file, const std::string& func,
+        unsigned line, const std::string& hint) 
     {
         if (!t) {
-            cerr << file << "("s << line << "): "s << func << ": "s << "ASSERT(" << t_str << ") failed.";
+            std::cerr << file << "(" << line << "): " << func << ": " << "ASSERT(" << t_str << ") failed.";
             if (!hint.empty()) {
-                cerr << " Hint: " << hint;
+                std::cerr << " Hint: " << hint;
             }
-            cerr << endl;
+            std::cerr << std::endl;
             abort();
         }
     }
 
-    #define ASSERT(expr) AssertImpl(expr, #expr, __FILE__, __FUNCTION__, __LINE__, ""s)
-    #define ASSERT_HINT(expr, hint) AssertImpl(expr, #expr, __FILE__, __FUNCTION__, __LINE__, hint)
+    #define ASSERT(expr) AssertImpl((expr), #expr, __FILE__, __FUNCTION__, __LINE__, "")
+    #define ASSERT_HINT(expr, hint) AssertImpl((expr), #expr, __FILE__, __FUNCTION__, __LINE__, hint)
 
 
 
     // Тест-функция для макроса ASSERT_EQUAL
     template <typename T, typename U>
-    void AssertEqualImpl(const T& t, const U& u, const string& t_str, const string& u_str, const string& file,
-        const string& func, unsigned line, const string& hint) 
+    void AssertEqualImpl(const T& t, const U& u, const std::string& t_str, const std::string& u_str, const std::string& file,
+        const std::string& func, unsigned line, const std::string& hint) 
         {
         if (t != u) {
-            cerr << boolalpha;
-            cerr << file << "("s << line << "): "s << func << ": "s;
-            cerr << "ASSERT_EQUAL("s << t_str << ", "s << u_str << ") failed: "s;
-            cout << t << " != "s << u << "."s;
+            std::cerr << std::boolalpha;
+            std::cerr << file << "(" << line << "): " << func << ": ";
+            std::cerr << "ASSERT_EQUAL(" << t_str << ", " << u_str << ") failed: ";
+            std::cerr << t << " != " << u << ".";
             if (!hint.empty()) {
-                cerr << " Hint: "s << hint;
+                std::cerr << " Hint: " << hint;
             }
-            cerr << endl;
+            std::cerr << std::endl;
             abort();
         }
     }
 
-    #define ASSERT_EQUAL(a, b) AssertEqualImpl((a), (b), #a, #b, __FILE__, __FUNCTION__, __LINE__, ""s)
+    #define ASSERT_EQUAL(a, b) AssertEqualImpl((a), (b), #a, #b, __FILE__, __FUNCTION__, __LINE__, "")
     #define ASSERT_EQUAL_HINT(a, b, hint) AssertEqualImpl((a), (b), #a, #b, __FILE__, __FUNCTION__, __LINE__, (hint))
 
 
 
     // Функция для запуска теста через макрос RUN_TEST
     template <typename TestFunction>
-    void RunTestImpl(TestFunction test_func, string test_function_string) {
+    void RunTestImpl(TestFunction test_func, std::string test_function_string) {
         test_func();
-        std::cerr << test_function_string << " OK" << std::endl;
+        std::cout << test_function_string << " OK" << std::endl;
     }
 
     #define RUN_TEST(func)  RunTestImpl(func, #func);
@@ -75,14 +72,14 @@ namespace Test_SearchServer
     // Тест проверяет, что поисковая система исключает стоп-слова при добавлении документов
     void TestExcludeStopWordsFromAddedDocumentContent() {
         const int doc_id = 1;
-        const string content = "cat in the city"s;
-        const vector<int> ratings = {1, 2, 3};
+        const std::string content = "cat in the city";
+        const std::vector<int> ratings = {1, 2, 3};
 
         // Документы со стоп-словами находятся
         {
             SearchServer server;
             server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
-            const auto found_docs = server.FindTopDocuments("in"s);
+            const auto found_docs = server.FindTopDocuments("in");
             ASSERT_EQUAL(found_docs.size(), 1u);
             const Document& doc0 = found_docs[0];
             ASSERT_EQUAL(doc0.id, doc_id);
@@ -91,9 +88,9 @@ namespace Test_SearchServer
         // Документы по запросу, содержищие стоп-слова, не находятся
         {
             SearchServer server;
-            server.SetStopWords("in the"s);
+            server.SetStopWords("in the");
             server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
-            ASSERT_HINT(server.FindTopDocuments("in"s).empty(), "Stop words must be excluded from documents"s);
+            ASSERT_HINT(server.FindTopDocuments("in").empty(), "Stop words must be excluded from documents");
         }
     }
 
@@ -101,15 +98,15 @@ namespace Test_SearchServer
     void TestAddDocumentWithQueryWords()
     {
         const int doc_id = 1;
-        const string content = "cat in the city"s;
-        const vector<int> ratings = {1, 2, 3};
+        const std::string content = "cat in the city";
+        const std::vector<int> ratings = {1, 2, 3};
 
         // Проверяем, что если содержимое документа не пересекается с запросом, то в результате выполнения запроса
         // ничего не находится
         {
             SearchServer server;
             server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
-            const auto found_docs = server.FindTopDocuments("and"s);
+            const auto found_docs = server.FindTopDocuments("and");
             ASSERT_EQUAL(found_docs.size(), 0u);
         }
 
@@ -118,7 +115,7 @@ namespace Test_SearchServer
         {
             SearchServer server;
             server.AddDocument(doc_id, content, DocumentStatus::BANNED, ratings);
-            const auto found_docs = server.FindTopDocuments("cat in the"s);
+            const auto found_docs = server.FindTopDocuments("cat in the");
             ASSERT_EQUAL(found_docs.size(), 0u);
         }
 
@@ -127,13 +124,13 @@ namespace Test_SearchServer
         {
             SearchServer server;
             server.AddDocument(doc_id, content, DocumentStatus::BANNED, ratings);
-            const auto found_docs = server.FindTopDocuments("cat in"s, DocumentStatus::BANNED);
+            const auto found_docs = server.FindTopDocuments("cat in", DocumentStatus::BANNED);
             ASSERT_EQUAL(found_docs.size(), 1u);
             const Document& doc0 = found_docs[0];
             ASSERT_EQUAL(doc0.id, doc_id);
 
             // Проверка на предикат-функцию
-            const auto found_docs1 = server.FindTopDocuments("cat in"s, [](int document_id, DocumentStatus status, int rating) { return document_id % 2 == 1;});
+            const auto found_docs1 = server.FindTopDocuments("cat in", [](int document_id, DocumentStatus status, int rating) { return document_id % 2 == 1;});
             ASSERT_EQUAL(found_docs.size(), 1u);
             const Document& doc01 = found_docs[0];
             ASSERT_EQUAL(doc01.id, doc_id);
@@ -143,7 +140,7 @@ namespace Test_SearchServer
         {
             SearchServer server;
             server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
-            const auto found_docs = server.FindTopDocuments("cat in"s);
+            const auto found_docs = server.FindTopDocuments("cat in");
             ASSERT_EQUAL(found_docs.size(), 1u);
             const Document& doc0 = found_docs[0];
             ASSERT_EQUAL(doc0.id, doc_id);
@@ -154,15 +151,15 @@ namespace Test_SearchServer
     void TestMinusWordsNotInTheResult()
     {
         const int doc_id_1 = 1, doc_2 = 2;
-        const string content1 = "cat in the city"s, content2 = "bee in the boat"s;
-        const vector<int> ratings1 = {1, 2, 3}, ratings2 = {-1, -2, -3};
+        const std::string content1 = "cat in the city", content2 = "bee in the boat";
+        const std::vector<int> ratings1 = {1, 2, 3}, ratings2 = {-1, -2, -3};
         
         // Проверяем, что документ без минусов слов находится, а с минус-словами - нет
         {
             SearchServer server;
             server.AddDocument(doc_id_1, content1, DocumentStatus::ACTUAL, ratings1);
             server.AddDocument(doc_2, content2, DocumentStatus::ACTUAL, ratings2);
-            const auto found_docs = server.FindTopDocuments("in the -bee"s);
+            const auto found_docs = server.FindTopDocuments("in the -bee");
             ASSERT_EQUAL(found_docs.size(), 1u);
             const Document& doc0 = found_docs[0];
             ASSERT_EQUAL(doc0.id, doc_id_1);
@@ -173,8 +170,8 @@ namespace Test_SearchServer
     void TestDocumentMatching()
     {
         const int doc_id1 = 1, doc_id2 = 2, doc_id3 = 3;
-        const string content1 = "cat in the city"s, content2 = "bee in the boat"s, content3 = "dog in da home"s;
-        const vector<int> ratings = {1, 2, 3};
+        const std::string content1 = "cat in the city", content2 = "bee in the boat", content3 = "dog in da home";
+        const std::vector<int> ratings = {1, 2, 3};
         
         // Проверка на полное соответсвие документов запросу 
         {
@@ -183,7 +180,7 @@ namespace Test_SearchServer
             server.AddDocument(doc_id2, content2, DocumentStatus::ACTUAL, ratings);
             server.AddDocument(doc_id3, content3, DocumentStatus::ACTUAL, ratings);
 
-            const auto found_docs = server.FindTopDocuments("in the -dog"s);
+            const auto found_docs = server.FindTopDocuments("in the -dog");
             ASSERT_EQUAL(found_docs.size(), 2u);
             const Document& doc0 = found_docs[0];
             const Document& doc1 = found_docs[1];
@@ -196,7 +193,7 @@ namespace Test_SearchServer
             SearchServer server;
             server.AddDocument(doc_id3, content3, DocumentStatus::ACTUAL, ratings);
 
-            const auto found_docs = server.FindTopDocuments("-in the -dog"s);
+            const auto found_docs = server.FindTopDocuments("-in the -dog");
             ASSERT_EQUAL(found_docs.size(), 0u);
         }
     }
@@ -204,11 +201,11 @@ namespace Test_SearchServer
     // Тест на сортировку по релевантности
     void TestFoundDocumentSortedByRelevance()
     {
-        const string stop_words = "и в на"s;
+        const std::string stop_words = "и в на";
         const size_t doc_count = 3;
         const int doc_id1 = 1, doc_id2 = 2, doc_id3 = 3;
-        const string content1 = "белый кот и модный ошейник"s, content2 = "пушистый кот пушистый хвост"s, content3 = "ухоженный пёс выразительные глаза"s;
-        const string query = "пушистый ухоженный кот"s;
+        const std::string content1 = "белый кот и модный ошейник", content2 = "пушистый кот пушистый хвост", content3 = "ухоженный пёс выразительные глаза";
+        const std::string query = "пушистый ухоженный кот";
 
         // Проверка на сортировку по релевантности документов (заранее посчитано)
         {
@@ -239,15 +236,15 @@ namespace Test_SearchServer
     void TestRatingOfTheDocument()
     {
         const int doc_id = 1;
-        const string content = "cat in the city"s;
-        const vector<int> ratings = {1, 2, 3, 4 ,5, 6, 7, 8};
+        const std::string content = "cat in the city";
+        const std::vector<int> ratings = {1, 2, 3, 4 ,5, 6, 7, 8};
 
         // Проверка на подсчет среднего рейтинга
         {
             SearchServer server;
             server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
             
-            const auto found_documents = server.FindTopDocuments("cat in the"s);
+            const auto found_documents = server.FindTopDocuments("cat in the");
             ASSERT_EQUAL(found_documents.size(), 1u);
             const Document& f_doc = found_documents[0];
             ASSERT_EQUAL(f_doc.rating, 4);
@@ -259,10 +256,10 @@ namespace Test_SearchServer
     // Тест на поиск документов с заданным статусом
     void TestFindDocumentByStatus()
     {
-        const string stop_words = "и в на"s;
+        const std::string stop_words = "и в на"s;
         const int doc_id1 = 1, doc_id2 = 2, doc_id3 = 3;
-        const string content1 = "белый кот и модный ошейник"s, content2 = "пушистый кот пушистый хвост"s, content3 = "ухоженный пёс выразительные глаза"s;
-        const string query = "пушистый ухоженный кот"s;
+        const std::string content1 = "белый кот и модный ошейник", content2 = "пушистый кот пушистый хвост", content3 = "ухоженный пёс выразительные глаза";
+        const std::string query = "пушистый ухоженный кот";
 
         // Проверка на статус по умолчанию
         {
@@ -296,11 +293,11 @@ namespace Test_SearchServer
     // Корректное вычисление релевантности системы
     void TestCorrectRelevanceOfTheSystem()
     {
-        const string stop_words = "и в на"s;
+        const std::string stop_words = "и в на";
         const size_t doc_count = 3;
         const int doc_id1 = 1, doc_id2 = 2, doc_id3 = 3;
-        const string content1 = "белый кот и модный ошейник"s, content2 = "пушистый кот пушистый хвост"s, content3 = "ухоженный пёс выразительные глаза"s;
-        const string query = "пушистый ухоженный кот"s;
+        const std::string content1 = "белый кот и модный ошейник", content2 = "пушистый кот пушистый хвост", content3 = "ухоженный пёс выразительные глаза";
+        const std::string query = "пушистый ухоженный кот";
 
         // Проверка на сортировку по релевантности документов (заранее посчитано)
         {
@@ -324,9 +321,9 @@ namespace Test_SearchServer
             ASSERT_EQUAL(doc2.rating, 3);
 
             const double EPS = 0.0001;
-            ASSERT(abs(doc0.relevance - 0.650672) < EPS);
-            ASSERT(abs(doc1.relevance - 0.274653) < EPS);
-            ASSERT(abs(doc2.relevance - 0.101366) < EPS);
+            ASSERT(std::abs(doc0.relevance - 0.650672) < EPS);
+            ASSERT(std::abs(doc1.relevance - 0.274653) < EPS);
+            ASSERT(std::abs(doc2.relevance - 0.101366) < EPS);
         }
     }
 
@@ -345,5 +342,3 @@ namespace Test_SearchServer
     // --------- Окончание модульных тестов поисковой системы -----------
 
 }
-
-#endif // !TEST_SEARCH_SERVER_HPP
