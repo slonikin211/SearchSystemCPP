03543be1388293d1d71c6ae7b70513fe46d9c027
slonikin211 <pakgeorge1209@gmail.com>
2021-09-10 18:55:25 +0300

    for new version

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 // After main development
 // Template definition in a .cpp file (using explicit instanciation)
 // 31.08.2021 - Duration test functionality
+// 10.09.2021 - Refactoring and class updated (begin and end instead of GetDocumentById, GetWordFrequencies, RemoveDocument, RemoveDuplicates)
 
 #include <iostream>
 using namespace std;
@@ -19,14 +20,12 @@ using namespace std;
 #include "include/Paginator.hpp"
 #include "include/RequestQueue.hpp"
 #include "include/LogDuration.hpp"
+#include "include/RemoveDuplicates.hpp"
 
 int main() {
     Test_SearchServer::TestSearchServer();
     // If you see this line, all tests are passed
     cout << "Search server testing finished"s << endl;
-
-    LOG_DURATION("Standart hello");
-    LOG_DURATION_STREAM("Hello from stream"s, cout);
 
     return 0;
 }
